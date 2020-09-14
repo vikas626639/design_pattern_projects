5af20345e21874d55b1b14d755dbd326073dfcc9
vikas yadav <vikas626639@gmail.com>
2020-09-14 20:36:19 +0530

    singleton with minimum standrd

--- proj01-singleton_with_eger_instantition/src/com/nit/dp/Printer.java
+++ proj01-singleton_with_eger_instantition/src/com/nit/dp/Printer.java
@@ -1,5 +1,22 @@
 package com.nit.dp;
 
 public class Printer {
+	
+		private static Printer instence=null;
 
-}
+		private Printer() {
+			System.out.println("0 param constructor");
+		}
+
+		public static Printer getInstance() {
+			//static factory method
+			if(instence==null) {
+				instence =new Printer();
+			}
+			return instence;
+		}
+		public void printDate(String data) {
+			System.out.println("user given data is ="+data);
+		}
+	}
+
--- proj01-singleton_with_eger_instantition/src/com/nit/test/PrinterTest.java
+++ proj01-singleton_with_eger_instantition/src/com/nit/test/PrinterTest.java
@@ -1,10 +1,18 @@
 package com.nit.test;
 
+import com.nit.dp.Printer;
+
 public class PrinterTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Printer p=null,p1=null;
+		//not possoible to create object outside the class
+		p=Printer.getInstance();
+		p1=Printer.getInstance();
+		System.out.println("hashcode of p = "+p.hashCode());
+		System.out.println("hashcode of p1= "+p1.hashCode());
+		System.out.println("p==p1?-->"+(p==p1));
+		System.out.println("============");
+		p.printDate("vikas yadav");
 	}
-
 }
