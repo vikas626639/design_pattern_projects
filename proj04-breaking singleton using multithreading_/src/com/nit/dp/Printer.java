5aef387e238d4a21086f680fead6eb15e56d0a1e
vikas yadav <vikas626639@gmail.com>
2020-09-15 16:02:27 +0530

    breaking singleton using multithreading

---
+++ proj04-breaking singleton using multithreading_/src/com/nit/dp/Printer.java
@@ -0,0 +1,28 @@
+package com.nit.dp;
+
+public class Printer {
+	static {
+		System.out.println("Printer class has been loaded");
+	}
+	
+		//using lazy instantition
+		private static Printer instence;
+
+		private Printer() {
+			System.out.println("0 param constructor");
+		}
+
+		//static factory method
+		public static Printer getInstance() {
+			if(instence==null) {
+				instence =new Printer();
+			}
+			return instence;
+		}
+		
+		public void printDate(String data) {
+			System.out.println("user given data is ="+data);
+		}
+
+
+}
