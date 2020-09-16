b6e1df8b571de5bf04d6fcfebf25e07af4e5c6aa
vikas yadav <vikas626639@gmail.com>
2020-09-16 13:39:23 +0530

    breaking singleton by cloning of object

---
+++ proj06-breaking_singlton_using_cloning/src/com/nit/dp/Printer.java
@@ -0,0 +1,38 @@
+package com.nit.dp;
+
+import com.nit.interface1.CloningPrinter;
+
+public class Printer implements CloningPrinter {
+	static {
+		System.out.println("Printer class has been loaded");
+	}
+	
+	//using lazy instantition
+	private static Printer instence;
+
+	private Printer() {
+		System.out.println("0 param constructor");
+	}
+	
+	//static factory method
+	public static Printer getInstance() {
+		if(instence==null) {					//first null check
+			synchronized (Printer.class) {
+				if(instence==null) {			//double null check
+					instence =new Printer();
+				}
+			}
+		}
+		return instence;
+	}
+	
+	@Override
+	public Object clone() throws CloneNotSupportedException {
+		System.out.println("clone method ");
+		return super.clone();
+	}
+
+	public void printDate(String data) {
+		System.out.println("user given data is ="+data);
+	}
+}
