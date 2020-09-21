2584a3076b8e20d9ca42b523a9ff1749251ec23c
vikas yadav <vikas626639@gmail.com>
2020-09-21 11:35:40 +0530

    the perfect singleton object using thraditional approach

---
+++ Proj12-the_perfect_singleton_class/src/com/nit/dp/Singleton.java
@@ -0,0 +1,47 @@
+package com.nit.dp;
+
+public class Singleton {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	//using minimum standard
+	private static Singleton instance;
+	private static boolean condition=false;
+
+	//defining private constructor
+	private Singleton() throws Exception {
+		if(condition==true) {
+			throw new InstantiationException("can not instantiate");
+		}
+		condition=true;
+		System.out.println("0 param constructor");
+	}
+
+	//egar instantiation
+	public static Singleton getinstance() throws Exception {
+		//first null checking
+		if(instance==null) {
+			synchronized (Singleton.class) {
+				//double null check
+				if(instance==null) {
+					instance=new Singleton();
+				}
+			}
+		}
+		return instance;
+	}
+
+	//cloning 
+	@Override
+	public Object clone() throws CloneNotSupportedException {
+		// TODO Auto-generated method stub
+		return instance;
+	}
+
+	//deserialization
+	public Object readResolve() {
+		System.out.println("calling readResolve method");
+		return instance;
+	}
+}
