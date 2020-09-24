ce3eb997c756db3cb924e3326005afce66ac5d85
vikas yadav <vikas626639@gmail.com>
2020-09-24 15:30:01 +0530

    ..........

--- proj06-breaking_singlton_using_cloning/src/com/nit/test/Test.java
+++ proj06-breaking_singlton_using_cloning/src/com/nit/test/Test.java
@@ -10,9 +10,12 @@ public class Test {
 	p2=Printer.getInstance();
 	System.out.println("hashcode of p1= "+p1.hashCode());
 	System.out.println("hashcode of p2= "+p2.hashCode());
+	System.out.println("hashcode of p1= "+p1.hashCode());
+	System.out.println("hashcode of p2= "+p2.hashCode());
+	System.out.println("hashcode of p1= "+p1.hashCode());
+	System.out.println("hashcode of p2= "+p2.hashCode());
 	System.out.println("p1==p2-->"+(p1==p2));
 	p3=(Printer) Printer.getInstance().clone();
 	System.out.println("hashcode of p3= "+p3.hashCode());
 	}
-
 }
